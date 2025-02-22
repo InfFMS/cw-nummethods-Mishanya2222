244bdebfbcca91351e5e45fce16d76440204b4f6
Mishanya2222 <146630550+Mishanya2222@users.noreply.github.com>
2025-02-22 11:16:12 +0700

    Update задание3.py
    исправил

--- задание3.py
+++ задание3.py
@@ -8,22 +8,22 @@ R = 8.31
 #[P + a (n2/V2)] (V - nb) = nRT
 a = 0.1382
 b = 3.19*10**(-5)
-T = -130
+T = 143.15
 L =0
 
 
 
 
 def eq(V):
-    P = -(R*T)/(V - b) - (a/(V**2))
+    P = (R*T)/(V - b) - (a/(V**2))
     return P
 
 
 
 
 
-V = 2.15*(10**(-5))
-while V < 0.0004:
+V = 3*(10**(-5))
+while V < 0.00018:
     V =V + b
     lst_y.append(eq(V))
     lst_x.append(V)
@@ -45,7 +45,7 @@ L = 0
 V = min_x
 while V < max_x:
     V = V + b
-    L = L + math.sqrt((-(R*T)/((V+b) - b) - (a/((V+b)**2)))**2 - -(R*T)/(V - b) - (a/(V**2)))
+    L = L + math.sqrt((-(R*T)/((V+b) - b) - (a/((V+b)**2)))**2 -(R*T)/(V - b) - (a/(V**2)))
 
 plt.plot(lst_x,lst_y)
 plt.show()
