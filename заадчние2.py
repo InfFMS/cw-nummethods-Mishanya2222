e936d48b03744299cec7dfc979916a5718400573
Mishanya2222 <146630550+Mishanya2222@users.noreply.github.com>
2025-02-22 10:58:48 +0700

    Update заадчние2.py
    сделал по человечески

--- заадчние2.py
+++ заадчние2.py
@@ -2,8 +2,8 @@ import numpy
 from matplotlib import pyplot as plt
 import  math
 lst_x = []
-
 lst_y = []
+
 R = 8.31
 #[P + a (n2/V2)] (V - nb) = nRT
 a = 0.1382
@@ -16,7 +16,7 @@ def eq(V):
 
 
 V = 2.15*(10**(-5))
-while V < 10**(-3):
+while V < 0.0004:
     V =V + b
     # t = ((+(R*T)/((V+b) - b) - (a/((V+b)**2))  -(R*T)/(V - b) - (a/(V**2))))/b
     # if t>0:
@@ -25,16 +25,19 @@ while V < 10**(-3):
     lst_x.append(V)
 
 num = 0
-min = 10000000000000000000000
-max = 0
+min_y = 10000000000000000000000
+max_y = 0
 for i in lst_y:
-    num = num + 1
-    if i > max:
-        max = lst_x[num-1]
-    if i < min:
-        min = lst_x[num-1]
+    if i > max_y:
+        max_y = i
+        max_x = lst_x[num]
+    if i < min_y:
+        min_y = i
+        min_x = lst_x[num]
 
-print(min,max)
+    num = num + 1
+
+print(min_x,max_x)
 
 plt.plot(lst_x,lst_y)
 plt.show()
